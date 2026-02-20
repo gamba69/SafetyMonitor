4ac5c30960a8daa10963592fe708d0ccc83278d8
Igor Dulevich <gamba69@gmail.com>
2026-02-20 10:11:42 +0200

    One instance

--- SafetyMonitorView/Program.cs
+++ SafetyMonitorView/Program.cs
@@ -1,12 +1,26 @@
 using SafetyMonitorView.Forms;
+using System.Threading;
 
 namespace SafetyMonitorView;
 
 static class Program {
+    private const string SingleInstanceMutexName = "SafetyMonitorView.SingleInstance";
+
     #region Private Methods
 
     [STAThread]
     static void Main() {
+        using Mutex singleInstanceMutex = new(false, SingleInstanceMutexName, out bool isFirstInstance);
+        if (!isFirstInstance) {
+            MessageBox.Show(
+                "The application is already running. Launching a second instance is not allowed.",
+                "Safety Monitor",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning
+            );
+            return;
+        }
+
         // Explicit DPI configuration before any controls are created
         Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
         Application.EnableVisualStyles();
